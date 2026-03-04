7d29024ae47f679a0e7a8e862030cc906739b8e0
GUGOFO <gugofogomes@gmail.com>
2026-03-04 19:15:51 -0300

    estilos

--- src/jsx/ToDoList/ToDoList.jsx
+++ src/jsx/ToDoList/ToDoList.jsx
@@ -1,9 +1,9 @@
-
+import Styles from "./ToDoList.module.css"
 import React, {useState} from "react"
 
 function ToDoList(){
 
-    const [tarefas, setTarefas] = useState(["Almoçar", "Tomar banho"]);
+    const [tarefas, setTarefas] = useState(["Almoçar", "Tomar banho", "Ser Gay"]);
     const [novaTarefa, setNovaTarefa] = useState("");
 
     function lidarInputChange(e){
@@ -27,22 +27,23 @@ function ToDoList(){
     }
 
     return(
-        <div className="toDoList">
+        <div className={Styles.toDoList}>
             <h1>Lista de Tarefas</h1>
 
             <div>
                 <input type="text" placeholder="Coloque uma tarefa" value={novaTarefa} onChange={lidarInputChange}/>
-                <button className="addButao" onClick={adicionarTarefas}>Add</button>
+                <button className={Styles.adicionar} onClick={adicionarTarefas}>Add</button>
             </div>
 
             <ol>
-                {tarefas.map((tarefa, index) => {
+                {tarefas.map((tarefa, index) => 
                     <li key={index}>
-                        <span className="texto">{tarefa}</span>
-                        <button className="deletar" onClick={(index) => deletarTarefa(index)}>Deletar</button>
-                        <button className="cima" onClick={(index) => moverTarefaCima(index)}>Cima</button>
-                        <button className="baixo" onClick={(index) => moverTarefaCima(index)}>Baixo</button>
-                    </li> })}
+                        <span className={Styles.texto}>{tarefa}</span>
+                        <button className={Styles.delete} onClick={deletarTarefa(index)}>Deletar</button>
+                        <button className={Styles.mover} onClick={moverTarefaCima(index)}>Cima</button>
+                        <button className={Styles.mover} onClick={moverTarefaBaixo(index)}>Baixo</button>
+                    </li>
+                )}
             </ol>
         </div>
     )
