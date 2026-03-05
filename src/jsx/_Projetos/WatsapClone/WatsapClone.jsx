5ed268c0bc78c67052273fc4cd296d13d48b3fd6
GUGOFO <gugofogomes@gmail.com>
2026-03-05 00:39:09 -0300

    logica com react quaase finalizada

--- src/jsx/_Projetos/WatsapClone/WatsapClone.jsx
+++ src/jsx/_Projetos/WatsapClone/WatsapClone.jsx
@@ -1,8 +1,27 @@
 import React, {useState} from "react";
 import Styles from "./WatsapClone.module.css"
 
-
 function WatsapClone(){
+
+    const [msgEnviada, setMsgEnviada] = useState("");
+    const [msgRecebida, setMsgRecebida] = useState("");
+    const [historicoConversas, setHistoricoConversas] = useState([{texto: "Oii", classe: "enviadorMsg"}, {texto: "E ai como vc esta?", classe: "recebidorMsg"}]);
+
+    function updateMsgEnviada(e){
+        setMsgEnviada(e.target.value);
+    }
+
+    function updateMsgRecebida(e){
+        setMsgRecebida(e.target.value);
+    }
+
+    function submitMsgEnviada(e){
+        e.preventDefault()
+    }
+
+    function submitMsgRecebida(e){
+        e.preventDefault()
+    }
 
     return(
         <div id={Styles.containerPrograma}>
@@ -10,17 +29,24 @@ function WatsapClone(){
             <div id={Styles.divConversas}>
                 <div className={Styles.enviadorMsg}></div>
                 <div className={Styles.recebidorMsg}></div>
+                {historicoConversas.map((mensagem, index) => 
+                    <div key={index} 
+                         className={mensagem.classe === "enviadorMsg" ? Styles.enviadorMsg : Styles.recebidorMsg}>
+                        
+                        {mensagem.texto}
+                    </div>
+                )}
             </div>
 
             <div id={Styles.divInputs}>
 
-                <form id={Styles.divRecebedorInput} className={Styles.divInput}>
-                    <input type="text" id={Styles.recebedorInput} autoComplete="off" placeholder="Responda a mensagem" />
+                <form id={Styles.divRecebedorInput} className={Styles.divInput} onSubmit={(e) => submitMsgRecebida(e)}>
+                    <input type="text" id={Styles.recebedorInput} onChange={(e) => updateMsgRecebida(e)} autoComplete="off" placeholder="Responda a mensagem" />
                     <input type="submit" id={Styles.recebedorBtn} value=">" />
                 </form>
 
-                <form id={Styles.divEnviadorInput} className={Styles.divInput}>
-                    <input type="text" id={Styles.enviadorInput} autoComplete="off" placeholder="Envia a mensagem" />
+                <form id={Styles.divEnviadorInput} className={Styles.divInput} onSubmit={(e) => submitMsgEnviada(e)}>
+                    <input type="text" id={Styles.enviadorInput} onChange={(e) => updateMsgEnviada(e)} autoComplete="off" placeholder="Envia a mensagem" />
                     <input type="submit" id={Styles.enviadorBtn} value=">" />
                 </form>
 
