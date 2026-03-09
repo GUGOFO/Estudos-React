dca33bdfca113b4cde6d8d66ebf8ee526eeab2a9
GUGOFO <gugofogomes@gmail.com>
2026-03-08 23:22:38 -0300

    identifico e modifico o valor dos numeros

--- src/jsx/_Projetos/BuscaBinaria/BuscaBinaria.jsx
+++ src/jsx/_Projetos/BuscaBinaria/BuscaBinaria.jsx
@@ -1,23 +1,35 @@
 import React, {useState, useEffect, useRef} from "react";
 import Styles from "./BuscaBinaria.module.css"
 
+
 function BuscaBinaria(){
-
+    
     const [numeros, setNumeros] = useState([0,0,0,0,0,0,0,0,0]);
-
-    console.log(numeros)
+    let index = -1;
+    
+    function modificação(e){
+        const inputDoUsuario = e.target;
+        const index = parseInt(inputDoUsuario.id.charAt(6));
+        const valor = parseInt(inputDoUsuario.value);
+    
+        setNumeros(n => {
+            const novoArray = [...n];
+            novoArray[index] = valor;
+            return novoArray;
+        });
+    }
 
     return(
         <div className="divBuscaBinaria">
             <h1 className="titulo">Busca Binaria interativa</h1>
 
             <div className={Styles.nodes}>
-                
+
                 {numeros.map((numero, id) => {
                     return (
                         <div className={Styles.node} key={id}>
                             <div className={Styles.setaParaBaixo}></div>
-                            <input type="number" className={Styles.numero} id={`numero-${id}`}/>
+                            <input type="number" className={Styles.numero} id={`numero${++index}`} placeholder="0" onChange={(e) => modificação(e)}/>
                         </div>
                     )
                 })}
