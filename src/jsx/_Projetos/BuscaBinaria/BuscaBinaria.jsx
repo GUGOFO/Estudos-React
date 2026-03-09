50b9c04253b7146f91c8997dc4d92f222ebcd894
Gustavo <gugofogomes@gmail.com>
2026-03-09 14:04:18 -0300

    Reset e Alerta

--- src/jsx/_Projetos/BuscaBinaria/BuscaBinaria.jsx
+++ src/jsx/_Projetos/BuscaBinaria/BuscaBinaria.jsx
@@ -6,7 +6,8 @@ function BuscaBinaria(){
     
     const [numeros, setNumeros] = useState([NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN]);
     const [numeroEscolhido, setNumeroEscolhido] = useState(NaN);
-    let index = -1;
+    const [desabilitados, setDesabilitados] = useState([false,false,true]);
+    const [visibilidadeAlerta, setVisibilidadeAlerta] = useState("none");
     
     function modificação(e){
         const inputDoUsuario = e.target;
@@ -25,22 +26,29 @@ function BuscaBinaria(){
         setNumeroEscolhido(n => n = valor);
     }
 
+    function ordemIncorreta(){
+        for(let i = 0; i < numeros.length - 2; i++)
+            if(numeros[i] >= numeros[i + 1]) return true;
+        return false;
+    }
+
     function rodar(){
         let numeroNaoDefinido = 0;
         numeros.forEach(numero => { if(Number.isNaN(numero)) numeroNaoDefinido++})
         
         if(numeroNaoDefinido || ordemIncorreta() || Number.isNaN(numeroEscolhido)){
-            console.log(ordemIncorreta())
-            console.log(numeroNaoDefinido);
-            console.log(numeroEscolhido);
-            console.log(numeros)
+            setVisibilidadeAlerta("flex")
+        }
+        else{
+            setVisibilidadeAlerta("none")
+
+            
         }
     }
 
-    function ordemIncorreta(){
-        for(let i = 0; i < numeros.length - 2; i++)
-            if(numeros[i] >= numeros[i + 1]) return true;
-        return false;
+    function resetar(){
+        setNumeros([NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN]);
+        setNumeroEscolhido(NaN);
     }
 
     return(
@@ -52,21 +60,39 @@ function BuscaBinaria(){
                 {numeros.map((numero, id) => {
                     return (
                         <div className={Styles.node} key={id}>
-                            <div className={Styles.setaParaBaixo} style={index + 1 != 4 ? {visibility: "hidden"} : {display: "visible"}} ></div>
-                            <input type="number" className={Styles.numero} id={`numero${++index}`} placeholder="0" onChange={(e) => modificação(e)}/>
+                            <div className={Styles.setaParaBaixo} style={id + 1 != 4 ? {visibility: "hidden"} : {visibility: "visible"}} ></div>
+                            <input  type="number"
+                                    className={Styles.numero} 
+                                    id={`numero${id}`}
+                                    placeholder="0"
+                                    onChange={(e) => modificação(e)}
+                                    value={Number.isNaN(numero) ? "" : numero}
+                            />
                         </div>
                     )
                 })}
 
             </div>
+
+            <p id={Styles.alerta} style={{display: visibilidadeAlerta}}>
+                Ocorreu um erro no seu programa, veja se: <br />
+                1 - os numeros existem <br />
+                2 - estão em ordem crescente <br />
+            </p>
             
             <h2>Coloque o numero que deseja buscar</h2>
-            <input type="number" className={Styles.numero} name="inputUsuario" placeholder="0" onChange={(e) => modificarEscolha(e)}/>
+            <input  type="number" 
+                    className={Styles.numero}
+                    name="inputUsuario" 
+                    placeholder="0" 
+                    onChange={(e) => modificarEscolha(e)} 
+                    value={Number.isNaN(numeroEscolhido) ? "" : numeroEscolhido}
+            />
 
             <div className={Styles.botoes}>
-                <button id={Styles.resetar} >RESETAR</button>
-                <button id={Styles.rodar} onClick={() => rodar()}>RODAR</button>
-                <button id={Styles.proximo} disabled={true}>PROXIMO</button>
+                <button id={Styles.resetar} disabled={desabilitados[0]} onClick={() => resetar()} >RESETAR</button>
+                <button id={Styles.rodar} disabled={desabilitados[1]} onClick={() => rodar()}>RODAR</button>
+                <button id={Styles.proximo} disabled={desabilitados[2]}>PROXIMO</button>
             </div>
             
         </div>
