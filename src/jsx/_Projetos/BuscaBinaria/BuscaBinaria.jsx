06eb138964563d4e2278c9dcc316092d058895ab
GUGOFO <gugofogomes@gmail.com>
2026-03-08 23:00:46 -0300

    Adicionar quantidade certa de caixas

--- src/jsx/_Projetos/BuscaBinaria/BuscaBinaria.jsx
+++ src/jsx/_Projetos/BuscaBinaria/BuscaBinaria.jsx
@@ -3,31 +3,24 @@ import Styles from "./BuscaBinaria.module.css"
 
 function BuscaBinaria(){
 
+    const [numeros, setNumeros] = useState([0,0,0,0,0,0,0,0,0]);
+
+    console.log(numeros)
+
     return(
         <div className="divBuscaBinaria">
             <h1 className="titulo">Busca Binaria interativa</h1>
 
             <div className={Styles.nodes}>
-
-                <div className={Styles.node} id="node1">
-                    <div className={Styles.setaParaBaixo}></div>
-                    <input type="number" id={Styles.numero} />
-                </div>
-
-                <div className={Styles.node} id="node1">
-                    <div className={Styles.setaParaBaixo}></div>
-                    <input type="number" id={Styles.numero} />
-                </div>
-
-                <div className={Styles.node} id="node1">
-                    <div className={Styles.setaParaBaixo}></div>
-                    <input type="number" id={Styles.numero} />
-                </div>
-
-                <div className={Styles.node} id="node1">
-                    <div className={Styles.setaParaBaixo}></div>
-                    <input type="number" id={Styles.numero} />
-                </div>
+                
+                {numeros.map((numero, id) => {
+                    return (
+                        <div className={Styles.node} key={id}>
+                            <div className={Styles.setaParaBaixo}></div>
+                            <input type="number" className={Styles.numero} id={`numero-${id}`}/>
+                        </div>
+                    )
+                })}
 
             </div>
         </div>
