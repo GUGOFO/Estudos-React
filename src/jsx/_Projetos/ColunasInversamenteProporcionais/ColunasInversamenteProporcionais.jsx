335315c788bb85ad630a955947c7c6ba56da3923
Gustavo <gugofogomes@gmail.com>
2026-03-11 23:36:37 -0300

    botão adicionar funciionando

--- src/jsx/_Projetos/ColunasInversamenteProporcionais/ColunasInversamenteProporcionais.jsx
+++ src/jsx/_Projetos/ColunasInversamenteProporcionais/ColunasInversamenteProporcionais.jsx
@@ -4,15 +4,25 @@ import Styles from "./ColunasInversamenteProporcionais.module.css";
 function ColunasInversamenteProporcionais(){
 
     const [colunas, setColunas] = useState([]);
+    const [tamanhoTotal, setTamanhoTotal] = useState(1);
     const [novoValor, setNovoValor] = useState(1);
 
     function atualizarValor(e){
-        const valorAtualizado = e.target.value;
+        const valorAtualizado = Number(e.target.value);
         if(valorAtualizado > 0){
             setNovoValor(n => n = valorAtualizado);
             console.log(valorAtualizado)
         }
     }
+
+    function adicionar(){
+        setColunas(c => c = [...c, novoValor]);
+        setNovoValor(1);
+    }
+
+    useEffect(() => {
+        colunas.length != 0 && setTamanhoTotal(t => t = colunas.reduce(soma))
+    }, [colunas])
 
     function soma(total, elemento){
         return total + elemento;
@@ -23,12 +33,12 @@ function ColunasInversamenteProporcionais(){
             <div className={Styles.divColunas}>
                 {colunas.map((coluna, id) => { return <div className={Styles.coluna} 
                                                         key={id} 
-                                                        style={{ "--altura" : `${(coluna/colunas.reduce(soma)) * 100}%` }}>
+                                                        style={{ "--altura" : `${(coluna/tamanhoTotal) * 100}%` }}>
                 </div> })}
 
             </div>
             <input type="number" id={Styles.input} min={1} value={novoValor} onChange={(e) => atualizarValor(e)}/>
-            <button id={Styles.btn}>+</button>
+            <button id={Styles.btn} onClick={() => adicionar()}>+</button>
         </div>
     )
 
