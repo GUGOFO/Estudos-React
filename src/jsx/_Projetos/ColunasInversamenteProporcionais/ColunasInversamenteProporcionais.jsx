4ab11fa8ed3ea0663a7a4d27f1cf4ab5d2fd7dc8
Gustavo <gugofogomes@gmail.com>
2026-03-11 23:11:50 -0300

    Colunas proporcionais

--- src/jsx/_Projetos/ColunasInversamenteProporcionais/ColunasInversamenteProporcionais.jsx
+++ src/jsx/_Projetos/ColunasInversamenteProporcionais/ColunasInversamenteProporcionais.jsx
@@ -1,6 +1,26 @@
 import React , {useState, useRef, useEffect} from "react";
+import Styles from "./ColunasInversamenteProporcionais.module.css";
 
 function ColunasInversamenteProporcionais(){
+
+    const [colunas, setColunas] = useState([]);
+
+    function soma(total, elemento){
+        return total + elemento;
+    }
+
+    return(
+        <div > 
+            <div className={Styles.divColunas}>
+                {colunas.map((coluna, id) => { return <div className={Styles.coluna} 
+                                                        key={id} 
+                                                        style={{ "--altura" : `${(coluna/colunas.reduce(soma)) * 100}%` }}>
+                </div> })}
+
+            </div>
+            <input type="number" />
+        </div>
+    )
 
 }
 
